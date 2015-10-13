5f7fdf221e3e0909454e716da831c84fcfbcea8c
Matthew Stickney <fiendishlinux@gmail.com>
2015-10-12 22:01:29 -0400

    Export the controller var.
    This is necessary because it will need to be closed-over and rebound to
    re-establish the control context in e.g. delayed-response lambdas. Don't
    really want to rebind unexported symbols, so export it.

--- package.lisp
+++ package.lisp
@@ -3,5 +3,6 @@
 (defpackage #:session-control
   (:use #:cl)
   (:export #:*session-control-middleware*
+           #:*session-controller*
            #:destroy-session
            #:change-session-id))
